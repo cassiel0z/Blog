49042b34539665b3663789058cbe9433acfc3e0b
charles_stark <1614869409@qq.com>
2023-05-17 15:36:29 +0800

    update

---
+++ blog_springboot/src/main/java/com/example/blog_springboot/BlogSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSpringbootApplication.class, args);
+    }
+
+}
